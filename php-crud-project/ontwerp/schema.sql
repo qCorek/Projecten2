77327b0746d3d84d296233f12dfe5d89e9ce0e63
Ali <l4nxgt@gmail.com>
2026-01-12 14:39:44 +0100

    Delete php-crud-project directory

--- php-crud-project/ontwerp/schema.sql
+++
@@ -1,8 +0,0 @@
--- SQLite schema (zelfde als TaskRepository::ensureSchema)
-CREATE TABLE IF NOT EXISTS tasks (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    description TEXT NULL,
-    is_done INTEGER NOT NULL DEFAULT 0,
-    created_at TEXT NOT NULL
-);
--- php-crud-project/src/templates/errors/404.php
+++
@@ -1,9 +0,0 @@
-<?php
-$title = 'Niet gevonden';
-ob_start();
-?>
-<h1>404 - Niet gevonden</h1>
-<p>De pagina bestaat niet.</p>
-<?php
-$content = ob_get_clean();
-require __DIR__ . '/_layout.php';
